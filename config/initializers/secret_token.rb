cfe3080211721cd5e35e9dcc0d527a57c96a3da9
Sreekanth <info@magnalia.co.in>
2011-07-09 13:54:18 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coupons::Application.config.secret_token = '343695cea933bacaa5c6cf55e59442283dacb665fdafd6fe594944a7e27e7f40a2956125d6cdc79106cd158e6699feb0d1fe310bf6531efc8bfbdc4ee5032cc2'
